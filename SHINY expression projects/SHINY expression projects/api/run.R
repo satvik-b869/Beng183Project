507b0674b4e683c118e3ed577c0df2ca33343988
Satvik Bandi <satvikcbandi@gmail.com>
2025-11-19 15:48:24 -0800

    Update DESeq2 endpoint and UI table display

--- SHINY expression projects/SHINY expression projects/api/run.R
+++ SHINY expression projects/SHINY expression projects/api/run.R
@@ -2,6 +2,7 @@ library(plumber)
 
 # Plumb the API from app.R
 pr <- plumber::plumb("app.R")
+pr <- pr_set_debug(pr, TRUE)   # <--- ADD THIS
 
 # Run on port 8000
 pr$run(host = "0.0.0.0", port = 8000)
